8726839790d8d5a5ff15c92a841533545ddf04dd
JULIANAMARTINET <martinetjuliana@gmail.com>
2022-05-06 21:38:25 +0200

    cambios en index

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector("#header-home");
     header.classList.toggle("down",window.scrollY>0);
 })  
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#thumbnail-carousel', {
+        //   fixedWidth : 200,
+          gap        : 3,
+          rewind     : true,
+          pagination : false,
+          perMove: 1,
+          type   : 'loop',
+          focus: 2 | 'center',
+  perPage: 4,
+  speed: 400,
+  autoplay: true,
+  interval: 3000
+
+
+    } ).mount();
+  } );
+
