7afdceb0fc4d7076c4b138995119e8c4ad28a836
JULIANAMARTINET <martinetjuliana@gmail.com>
2022-05-08 19:28:29 +0200

    formulario

--- php/form.php
+++ php/form.php
@@ -4,19 +4,23 @@
     $telefono = $_POST['telefono'];
     $mensaje = $_POST['textarea'];
 
+    $header = 'From: ' . $mail . " \r\n";
+    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
     $header .= "Content-Type: text/plain";
     
-    $mensaje = "Este mensaje fue enviado por: " . $nombre . ",\r\n";
-    $mensaje .= "Su mail es: " . $mail . ",\r\n";
+    $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+    $mensaje .= "Su e-mail es: " . $mail . " \r\n";
     $mensaje .= "Su telefono es: " . $telefono . ",\r\n" ;
-    $mensaje .= "Mensaje: " . $_POST ["mensaje"] . ",\r\n";
-    $mensaje .= "Enviado el: " . date('d/m/y', time()); 
+    $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+    $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-    $para = 'martinetjuliana@gmail.com';
+    $para = 'martinetjuliana@gmail.com';        
     $asunto = 'Este mail fue enviado desde la web';
 
     mail($para, $asunto, utf8_decode($mensaje), $header);
 
-    header('Location:../index.html');
+    header("Location:index.html");
+
 
 ?>
