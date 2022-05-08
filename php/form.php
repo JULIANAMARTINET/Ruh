5f3e492c3ec917ee3c20870430e23667548d18dd
JULIANAMARTINET <martinetjuliana@gmail.com>
2022-05-08 15:42:44 +0200

    funcionamiento del formulario

--- php/form.php
+++ php/form.php
@@ -4,16 +4,17 @@
     $telefono = $_POST['telefono'];
     $mensaje = $_POST['textarea'];
     
-$mensaje = "Este mensaje fue enviado por: " . $nombre . ",\r\n"
-$mensaje = "Su mail es: " . $mail . ",\r\n"
-$mensaje = "Su telefono es: " . $telefono . ",\r\n" 
-$mensaje = "Mensaje: " . $mensaje . ",\r\n"
-$mensaje = "Enviado el: " . date('d/m/y', time()); 
+$mensaje = "Este mensaje fue enviado por: " . $nombre . ",\r\n";
+$mensaje .= "Su mail es: " . $mail . ",\r\n";
+$mensaje .= "Su telefono es: " . $telefono . ",\r\n" ;
+$mensaje .= "Mensaje: " . $_POST ["mensaje"] . ",\r\n";
+$mensaje .= "Enviado el: " . date('d/m/y', time()); 
 
     $para = 'martinetjuliana@gmail.com';
     $asunto = 'Este mail fue enviado desde la web';
 
     mail($para, $asunto, utf8_decode($mensaje), $header);
 
-    header('Location:index.html');
+    header('Location:../index.html');
+
 ?>
