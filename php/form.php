8f37b6a58728d6d6d07104f33e7258647d869d31
JULIANAMARTINET <martinetjuliana@gmail.com>
2022-05-08 14:52:12 +0200

    Funcionamiento Form

--- php/form.php
+++ php/form.php
@@ -1,13 +1,19 @@
 <?php
     $nombre = $_POST['nombre'];
     $mail = $_POST['email'];
-    $telefono = $_POST['telefono']
+    $telefono = $_POST['telefono'];
     $mensaje = $_POST['textarea'];
     
+$mensaje = "Este mensaje fue enviado por: " . $nombre . ",\r\n"
+$mensaje = "Su mail es: " . $mail . ",\r\n"
+$mensaje = "Su telefono es: " . $telefono . ",\r\n" 
+$mensaje = "Mensaje: " . $mensaje . ",\r\n"
+$mensaje = "Enviado el: " . date('d/m/y', time()); 
+
     $para = 'martinetjuliana@gmail.com';
     $asunto = 'Este mail fue enviado desde la web';
 
-    mail($para, $asunto, utf8_decode($nombre, $mail, $telefono, $mensaje));
+    mail($para, $asunto, utf8_decode($mensaje), $header);
 
     header('Location:index.html');
 ?>
